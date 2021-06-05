6d76a5f7724efd7dcfea410762a17302f0839d9e
Anuja205 <82258854+Anuja205@users.noreply.github.com>
2021-06-05 13:07:07 +0530

    Projects folder added

---
+++ Projects/ClassLibrary1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary1;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Class1 c = new Class1();
+            Label1.Text=c.Check();
+        }
+    }
+}
